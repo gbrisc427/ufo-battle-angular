78dadbd273dbf49c9c20fe9153f183e94559edb7
gbrisc <gabrisc427@gmail.com>
2025-12-01 11:21:05 +0100

    Estructura Componentes

--- ufo-battle-angular/src/app/app.routes.ts
+++ ufo-battle-angular/src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { PlayComponent } from './pages/play/play.component';
+import { LoginComponent } from './pages/login/login.component';
+import { RegisterComponent } from './pages/register/register.component';
+import { SettingsComponent } from './pages/settings/settings.component';
+import { RankingsComponent } from './pages/rankings/rankings.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+{ path: '', component: HomeComponent },
+{ path: 'play', component: PlayComponent },
+{ path: 'login', component: LoginComponent },
+{ path: 'register', component: RegisterComponent },
+{ path: 'settings', component: SettingsComponent },
+{ path: 'rankings', component: RankingsComponent },
+{ path: '**', redirectTo: '' }
+];
